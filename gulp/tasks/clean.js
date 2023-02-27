1d70c2eb6d797f93eedec6ce527869bf9010471d
Medbzz <medbouazzi44@gmail.com>
2023-02-27 10:44:27 +0100

    fix gulp files cleaner

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -1,8 +1,12 @@
 const { src } = require('gulp');
 const paths = require('../paths');
 const cleaner = require('gulp-rimraf')
+const fs = require("fs");
 
 function clean() {
+    if (!fs.existsSync(paths.clean)){
+        fs.mkdirSync(paths.clean);
+    }
     return src(paths.clean, { read: false })
     .pipe(cleaner())
 };
